8297e1e7d8410590b06bc06dd6bd8f78c8ed9e66
peppecelesia03 <peppecelesia03@gmail.com>
2026-05-30 02:31:42 +0200

    prova

--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 DOMANDE = {
 
     1: "Il tuo personaggio è reale?",
-    2: "Il tuo personaggio"
+    2: "Il tuo personaggio indossa una maschera o no?",
+    3: "Il tuo personaggio usa molta tecnologia?",
+    4: "Il tuo personaggio ha superpoteri?"
 
 }
 
 DATABASE_PERSONAGGI = {
 
-    "Batman"
+    "Batman": {
+
+        "domande": {1: 0.01, 2: 0.99, 3: 0.95, 4: 0.10}g
+
+    }
 
 }
