5c983f4b4bb6536386e8125b542c9be461d30525
af-Alessandro <fiumefreddo.alessandro03@gmail.com>
2026-06-15 14:11:37 +0200

    Integra frontend con backend e Docker

---
+++ database.py
@@ -0,0 +1,8 @@
+DATABASE_PERSONAGGI = [
+
+    {"nome": "Batman",              "risposta":{1: 0.01, 2: 0.99, 3: 0.95, 4: 0.10}},
+    {"nome": "Superman",            "risposta":{1: 0.01, 2: 0.95, 3: 0.20, 4: 0.99}},
+    {"nome": "Elon Musk",           "risposta":{1: 0.99, 2: 0.05, 3: 0.90, 4: 0.01}},
+    {"nome": "Sherlock Holmes",     "risposta":{1: 0.05, 2: 0.10, 3: 0.05, 4: 0.01}}
+
+]
